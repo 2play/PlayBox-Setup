b11970d3d0b3b6f1d9f228bcef7a6bcf38f4b896
2play <sprafs@gmail.com>
2020-11-26 10:51:23 +0100

    Update post-fixes.sh

--- .pb-fixes/_scripts/post-fixes.sh
+++ .pb-fixes/_scripts/post-fixes.sh
@@ -1,5 +1,5 @@
 # The PlayBox Project
 # Copyright (C)2018-2020 2Play! (S.R.)
 # 25.11.2020
-echo Welcome to P;ayBox v2 Post Fixes & Tweaks
+echo "Welcome to PlayBox v2 Post Fixes & Tweaks"
 sleep 2
