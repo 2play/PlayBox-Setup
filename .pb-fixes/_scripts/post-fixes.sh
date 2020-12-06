aa11f75163a6105c5402f9c3a516c9be2ee117d0
2play <2playbuilds@gmail.com>
2020-12-07 00:57:00 +0100

    Update post-fixes.sh

--- .pb-fixes/_scripts/post-fixes.sh
+++ .pb-fixes/_scripts/post-fixes.sh
@@ -1,5 +1,14 @@
 # The PlayBox Project
 # Copyright (C)2018-2020 2Play! (S.R.)
-# 25.11.2020
+# 06.12.2020
 echo "Welcome to PlayBox v2 Post Fixes & Tweaks"
 sleep 2
+cd code
+git clone https://github.com/2play/PBv2-PostFixes.git
+cd PBv2-PostFixes
+rsync -urv --exclude '.git' --exclude 'LICENSE' --exclude 'README.md' . /
+echo
+echo "[OK DONE!...]"
+sleep 2
+cd ..
+rm -rf PBv2-PostFixes
