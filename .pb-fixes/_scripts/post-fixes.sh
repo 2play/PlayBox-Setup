00cff63af3a1914ea5db01a2840dfe36a66429f8
2play <2playbuilds@gmail.com>
2020-12-12 15:02:39 +0100

    Update post-fixes.sh

--- .pb-fixes/_scripts/post-fixes.sh
+++ .pb-fixes/_scripts/post-fixes.sh
@@ -6,10 +6,13 @@ sleep 2
 cd $HOME/code/
 git clone https://github.com/2play/PBv2-PostFixes.git
 cd PBv2-PostFixes/
-rsync -urv --exclude '.git' --exclude 'LICENSE' --exclude 'README.md' . /
+rsync -urv --exclude '.git' --exclude 'etc' --exclude 'LICENSE' --exclude 'README.md' . /
+sleep 1
+cd etc/
+sudo rsync -urv . /
 echo
 echo "[OK DONE!...]"
 sleep 2
-cd ..
+cd ../..
 rm -rf PBv2-PostFixes
 cd $HOME
