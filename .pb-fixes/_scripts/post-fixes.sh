e2857ad88e6591bfc6a6bf26d98392ec3412f4bd
2play <2playbuilds@gmail.com>
2021-01-05 13:43:33 +0100

    Update post-fixes.sh

--- .pb-fixes/_scripts/post-fixes.sh
+++ .pb-fixes/_scripts/post-fixes.sh
@@ -1,6 +1,6 @@
 # The PlayBox Project
 # Copyright (C)2018-2020 2Play! (S.R.)
-pb_version="PlayBox v2 Post Updates & Fixes: Dated 02.01.2021"
+pb_version="PlayBox v2 Post Updates & Fixes: Dated 05.01.2021"
 echo $pb_version
 sleep 3
 mkdir /home/pi/lmp4
