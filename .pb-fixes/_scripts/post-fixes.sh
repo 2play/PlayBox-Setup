41648dd8254a250506340c4cde198b173611e309
2play <2playbuilds@gmail.com>
2021-01-05 13:45:03 +0100

    Update post-fixes.sh

--- .pb-fixes/_scripts/post-fixes.sh
+++ .pb-fixes/_scripts/post-fixes.sh
@@ -154,7 +154,7 @@ sleep 1
 echo ""
 echo "[OK DONE!...]"
 cd $HOME
-exit
+return
 }
 
 function glb_shoff() {
@@ -166,7 +166,7 @@ sleep 1
 echo ""
 echo "[OK DONE!...]"
 cd $HOME
-exit
+return
 }
 
 global_shader
