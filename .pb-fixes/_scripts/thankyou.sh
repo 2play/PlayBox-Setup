9a3e8fceb3a16e30802e8745cfc99ce3894af493
2play <2playbuilds@gmail.com>
2021-03-30 14:14:59 +0200

    Update thankyou.sh

--- .pb-fixes/_scripts/thankyou.sh
+++ .pb-fixes/_scripts/thankyou.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # The PlayBox Project
 # Copyright (C)2018-2020 2Play! (S.R.)
-# 25.10.2020
+# 30.03.2021
 
 infobox=""
-infobox="${infobox}\n*** 2Play! Top Supporters & Friends***:\n- Jahitu, Gomer, Aeonis, Traxis, Yelah05, BMP, Randyslim, Strunx, bluestang, Sunrise169, Tisusbr, ChrisKerkides, Grumpa, TechXero, Mrnebu, Edale, haxel66 and of course all supporters!\n\n
+infobox="${infobox}\n*** 2Play! Top Supporters & Friends***:\n- Jahitu, Gomer, bluestang, QUIZASERAQ, Aeonis, Traxis, Yelah05, BMP, Randyslim, Strunx, Sunrise169, Tisusbr, ChrisKerkides, Grumpa, TechXero, Mrnebu, Edale and of course any MEGA supporters!\n\n
 
 *** Developers & Scripts ***:\n
 Demetris (Tinker PlayBox), rpc80 (Ryan Connors), EasyHax (aka Forrest), CyperGhost, David Marti (Motion Blue), Meleu, Lars Muldjord (Skyskraper), Bezel Project Team.\n\n
@@ -13,8 +13,8 @@ Demetris (Tinker PlayBox), rpc80 (Ryan Connors), EasyHax (aka Forrest), CyperGho
 Raspberry Pi Foundation for the nice clean OS.\n
 Retropie Team (Retropie), Andrew Mickelson (Attract Mode), HyperPie Team (HyperPie), Mátyás Mustoha (Pegasus-FE).\n\n
 
-*** Themes ***:\n
-Chicuelo, Ricky Romero, Rachid Lotf and all devianart\art creators.\n\n
+*** Themes & Art Ideas ***:\n
+Chicuelo & c64-dev, GeekOB, Ricky Romero, TreyM, Mr_RetroLust, Rachid Lotf and all devianart\art creators.\n\n
 
 *** Builders ***:\n
 Rick Dangerous, VirtualMan\n
