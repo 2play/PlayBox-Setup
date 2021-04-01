94401e10429d10987970dc62a40b24617c9e2ddd
2play <2playbuilds@gmail.com>
2021-04-01 23:24:30 +0200

    Update thankyou.sh

--- .pb-fixes/_scripts/thankyou.sh
+++ .pb-fixes/_scripts/thankyou.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # The PlayBox Project
 # Copyright (C)2018-2020 2Play! (S.R.)
-# 30.03.2021
+# 01.04.2021
 
 infobox=""
-infobox="${infobox}\n*** 2Play! Top Supporters & Friends***:\n- Jahitu, Gomer, bluestang, QUIZASERAQ, Aeonis, Traxis, Yelah05, BMP, Randyslim, Strunx, Sunrise169, Tisusbr, ChrisKerkides, Grumpa, TechXero, Mrnebu, Edale and of course any MEGA supporters!\n\n
+infobox="${infobox}\n*** 2Play! Top Supporters & Friends***:\n- Jahitu, Gomer, bluestang, QUIZASERAQ, Aeonis, Traxis, Yelah05, BMP, Randyslim, Strunx, Sunrise169, Tisusbr, dbresson, Everblight82, Eliasblizzard, ChrisKerkides, Grumpa, TechXero, Mrnebu, Edale and of course any MEGA supporters!\n\n
 
 *** Developers & Scripts ***:\n
 Demetris (Tinker PlayBox), rpc80 (Ryan Connors), EasyHax (aka Forrest), CyperGhost, David Marti (Motion Blue), Meleu, Lars Muldjord (Skyskraper), Bezel Project Team.\n\n
@@ -17,7 +17,7 @@ Retropie Team (Retropie), Andrew Mickelson (Attract Mode), HyperPie Team (HyperP
 Chicuelo & c64-dev, GeekOB, Ricky Romero, TreyM, Mr_RetroLust, Rachid Lotf and all devianart\art creators.\n\n
 
 *** Builders ***:\n
-Rick Dangerous, VirtualMan\n
+Rick Dangerous, VirtualMan, Schmoomer\n
 "
 infobox="${infobox}\n"
 
