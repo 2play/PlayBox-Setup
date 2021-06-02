0c06daa7701a5641d82d1d20c3aa9e0b053c34b9
2play <2playbuilds@gmail.com>
2021-06-02 19:17:40 +0200

    Update thankyou.sh

--- .pb-fixes/_scripts/thankyou.sh
+++ .pb-fixes/_scripts/thankyou.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # The PlayBox Project
 # Copyright (C)2018-2020 2Play! (S.R.)
-# 01.04.2021
+# 01.06.2021
 
 infobox=""
 infobox="${infobox}\n*** 2Play! Top Supporters & Friends***:\n- Jahitu, Gomer, bluestang, QUIZASERAQ, Aeonis, Traxis, Yelah05, BMP, Randyslim, Strunx, Sunrise169, Tisusbr, dbresson, Everblight82, Eliasblizzard, ChrisKerkides, Grumpa, TechXero, Mrnebu, Edale and of course any MEGA supporters!\n\n
 
 *** Developers & Scripts ***:\n
-Demetris (Tinker PlayBox), rpc80 (Ryan Connors), EasyHax (aka Forrest), CyperGhost, David Marti (Motion Blue), Meleu, Lars Muldjord (Skyskraper), Bezel Project Team.\n\n
+Demetris (Tinker PlayBox), rpc80 (Ryan Connors), Folly, EasyHax (aka Forrest), CyperGhost, David Marti (Motion Blue), Meleu, Lars Muldjord (Skyskraper), Bezel Project Team, Valerino.\n\n
 
 *** OS & FrontEnd ***:\n
 Raspberry Pi Foundation for the nice clean OS.\n
