5a91614558ed1fafd3f175fabe50dcdfed46cc13
2play <2playbuilds@gmail.com>
2022-04-03 22:21:50 +0200

    Update thankyou.sh

--- .pb-fixes/_scripts/thankyou.sh
+++ .pb-fixes/_scripts/thankyou.sh
@@ -10,7 +10,7 @@ infobox="${infobox}\n*** 2Play! Top Supporters & Friends***:\n- Jahitu, Gomer, B
 Rpc80 (Ryan Connors), Folly, Rydra, EasyHax (aka Forrest), Demetris Ierokipides aka Ntemis, CyperGhost, David Marti (Motion Blue), Meleu, Lars Muldjord (Skyskraper), Bezel Project Team, Valerino, Aditya Shakya aka adi1090x.\n\n
 
 *** OS & FrontEnd ***:\n
-Raspberry Pi Foundation for the nice clean OS.\n
+Armbian Team & Raspberry Pi Foundation for OS!.\n
 Retropie Team (Retropie), Andrew Mickelson (Attract Mode), HyperPie Team (HyperPie), Mátyás Mustoha (Pegasus-FE).\n\n
 
 *** Themes & Art Ideas ***:\n
