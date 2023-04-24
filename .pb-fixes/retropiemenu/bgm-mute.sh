6953670524cac4e23c3017bd46f09f6e448a793c
2play <2playbuilds@gmail.com>
2023-04-25 00:44:29 +0200

    Update bgm-mute.sh

--- .pb-fixes/retropiemenu/bgm-mute.sh
+++ .pb-fixes/retropiemenu/bgm-mute.sh
@@ -4,22 +4,8 @@
 # Copyright (C)2018-2023 2Play! (S.R.)
 # 24.04.2023
 
-infobox=""
-infobox="${infobox}\n"
-infobox="${infobox}Pi0 BGM Off/On\n\n"
-infobox="${infobox}\n"
-infobox="${infobox}This script will disable or enable the Pi0 BGM.\n"
-infobox="${infobox}\n"
-infobox="${infobox}\n\n"
-infobox="${infobox}**Enable**\nEnable option: The BGM is enabled after reboot."
-infobox="${infobox}\n"
-infobox="${infobox}**Disable**\nDisable option: the BGM is disabled after reboot.\n"
-infobox="${infobox}\n"
-infobox="${infobox}\n"
-
 dialog --backtitle "Pi0 BGM Off/On Switch" \
 --title "Pi0 BGM Off/On Switch by 2Play!" \
---msgbox "${infobox}" 35 110
 
 function main_menu() {
     local choice
@@ -27,7 +13,7 @@ function main_menu() {
     while true; do
         choice=$(dialog --backtitle "$BACKTITLE" --title " Pi0 BGM MENU " \
             --ok-label OK --cancel-label Exit \
-            --menu "What action would you like to perform?" 25 75 20 \
+            --menu "Which BGM option you want to apply?" 25 75 20 \
             - "*** Pi0 BGM SELECTIONS ***" \
             1 " - Enable Pi0 BGM" \
             2 " - Disable Pi0 BGM" \
@@ -48,7 +34,7 @@ function enable_BGM() {
 	clear
 	cp /opt/retropie/configs/all/autostartNOBGM.sh /opt/retropie/configs/all/autostart.sh
     clear
-    echo -e "\n\n\n                               Background Music Disabled\n\n\n"
+    echo -e "\n\n\n                Background Music Disabled\n\n\n"
     sleep 3
 	echo "Your Retropie is about to reboot so that the settings take effect!"
 	sudo reboot
@@ -58,7 +44,7 @@ function disable_BGM() {
 	dialog --infobox "...Removing..." 3 20 ; sleep 2
 	clear
 	cp /opt/retropie/configs/all/autostartBGM.sh /opt/retropie/configs/all/autostart.sh
-    echo -e "\n\n\n                                    Background Music Enabled\n\n\n"
+    echo -e "\n\n\n                Background Music Enabled\n\n\n"
     sleep 3
 	echo "Your Retropie is about to reboot so that the settings take effect!"
 	sudo reboot
