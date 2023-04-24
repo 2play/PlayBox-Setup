bf48d62c39994cac7cf5b45f9d763d1904bdb09a
2play <sprafs@gmail.com>
2023-04-24 17:58:32 +0200

    Pi0 Cleanup RPieMenu

--- .pb-fixes/retropiemenu/bgm-mute.sh
+++ .pb-fixes/retropiemenu/bgm-mute.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 stop_bgm(){
-        touch /home/pi/.DisableMusic
+        cp /opt/retropie/configs/all/autostartNOBGM.sh /opt/retropie/configs/all/autostart.sh
     clear
         echo -e "\n\n\n                               Background Music Disabled\n\n\n"
         sleep 3
 }
 
 start_bgm(){
-        rm /home/pi/.DisableMusic
+        cp /opt/retropie/configs/all/autostartBGM.sh /opt/retropie/configs/all/autostart.sh
         echo -e "\n\n\n                                    Background Music Enabled\n\n\n"
         sleep 3
 }
 
-if [ -e /home/pi/.DisableMusic ]; then
-        start_bgm
-else
-        stop_bgm
-fi
+	clear
+	echo "We need to restart system now..."
+	echo
+	read -n 1 -s -r -p "Press any key to continue..."
+	sleep 1
+	sudo reboot
+	echo
 exit
