26810aeb12076fbe8e1f45cf3621e6a89f91acae
2play <sprafs@gmail.com>
2023-05-23 16:46:22 +0200

    Update fixplaybox.sh

--- .pb-fixes/retropiemenu/fixplaybox.sh
+++ .pb-fixes/retropiemenu/fixplaybox.sh
@@ -627,8 +627,8 @@ function apps_pbt() {
 			4 " - Hide or Show a System " \
 			5 " - 2Play! Music Selections " \
 			6 " - Skyscraper By Lars Muldjord " \
-		    7 " - MESA & Vulkan Drivers Related Options " \
-		    8 " - [Disabled] PiKISS By Jose Cerrejon " \
+		    7 " - MESA & Vulkan Drivers Related Options [OFF] " \
+		    8 " - PiKISS By Jose Cerrejon [OFF] " \
 		    9 " - Single Saves Directory By RPC80 " \
 		   10 " - SD/USB Storage Benchmark " \
 		   11 " - OMXPlayer Volume Control Script " \
@@ -645,7 +645,7 @@ function apps_pbt() {
 			4) hd_sh_sys  ;;
 			5) music_2p  ;;
 			6) skyscraper  ;;
-			7) mesa_vk  ;;
+			#7) mesa_vk  ;;
 			#8) pikiss_git  ;;
 		    9) rpc80_saves  ;;
 		   10) strg_bench  ;;
@@ -3193,7 +3193,7 @@ function cl_saves() {
 	clear
 	find $HOME/RetroPie/roms/ -regextype posix-egrep -regex ".*\.(srm|auto|state.auto|fs|ldci|hi)$" -type f -delete
 	find $HOME/RetroPie/roms/daphne/ -regextype posix-egrep -regex ".*\.(srm|auto|state.auto|fs|hi|ldci|dat)$" -type f -delete
-	find $HOME/RetroPie/saves/ -regextype posix-egrep -regex ".*\.(srm|auto|state.auto|fs|hi|ldci|dat)$" -type f -delete
+	find $HOME/RetroPie/saves/ -regextype posix-egrep -regex ".*\.(srm|auto|state.auto|hi|ldci|dat)$" -type f -delete
 	clear
 	echo
 	echo "[OK DONE!...]"
