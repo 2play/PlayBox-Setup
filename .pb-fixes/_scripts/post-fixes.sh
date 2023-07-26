741c176fdb28d8c6f0e21819961917d97ef5824c
2play <sprafs@gmail.com>
2023-07-26 10:58:11 +0200

    Update post-fixes.sh

--- .pb-fixes/_scripts/post-fixes.sh
+++ .pb-fixes/_scripts/post-fixes.sh
@@ -143,8 +143,8 @@ fi
 # Fix NF Lolcat scrambled view...
 cd code
 wget https://github.com/busyloop/lolcat/archive/master.zip
-unzip master.zip
-rm master.zip
+unzip *master.zip
+rm *master.zip
 cd lolcat-master/bin
 sudo gem install lolcat
 cd ../.. && rm -rf lolcat-master
