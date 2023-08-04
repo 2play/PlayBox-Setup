75c8612b94035a3af8c643158c4dcaacbc22303a
2play <sprafs@gmail.com>
2023-08-04 12:26:03 +0200

    Update thankyou.sh

--- .pb-fixes/_scripts/thankyou.sh
+++ .pb-fixes/_scripts/thankyou.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # The PlayBox Project
-# Copyright (C)2018-2020 2Play! (S.R.)
-# 15.06.2021
+# Copyright (C)2018-2023 2Play! (S.R.)
+# 08.2023
 
 infobox=""
-infobox="${infobox}\n*** 2Play! Top Supporters & Friends***:\n- Jahitu, Gomer, BMP, bluestang, Randyslim, QUIZASERAQ, Tisusbr, dbresson, Everblight82, Eliasblizzard, Sunrise169, Yelah05, Aeonis, Traxis, Strunx, ChrisKerkides, Grumpa, TechXero, Mrnebu, Edale and of course any MEGA supporters!\n\n
+infobox="${infobox}\n*** 2Play! Top Supporters & Friends***:\n- Jahitu, BMP, bluestang, Randyslim, Tisusbr, Yelah05, Traxis, Strunx, ChrisKerkides,  TechXero, Mrnebu, Edale, Gomer, QUIZASERAQ, dbresson, Everblight82, Eliasblizzard, Sunrise169, Aeonis, Grumpa and of course any/all MEGA supporters!\n\n
 
 *** Developers & Scripts ***:\n
-Demetris (Tinker PlayBox), rpc80 (Ryan Connors), Folly, EasyHax (aka Forrest), CyperGhost, David Marti (Motion Blue), Meleu, Lars Muldjord (Skyskraper), Bezel Project Team, Valerino.\n\n
+Rpc80 (Ryan Connors), Rydra, EasyHax (aka Forrest), Demetris Ierokipides aka Ntemis, CyperGhost, Meleu, Lars Muldjord (Skyskraper), David Marti (Motion Blue), Bezel Project Team, Folly, Valerino, Aditya Shakya aka adi1090x, Paul Cercueil aka pcercuei.\n\n
 
 *** OS & FrontEnd ***:\n
 Raspberry Pi Foundation for the nice clean OS.\n
@@ -17,7 +17,7 @@ Retropie Team (Retropie), Andrew Mickelson (Attract Mode), HyperPie Team (HyperP
 Chicuelo & c64-dev, GeekOB, Ricky Romero, TreyM, Mr_RetroLust, Rachid Lotf and all devianart\art creators.\n\n
 
 *** Builders ***:\n
-Schmoomer, VirtualMan, Rick Dangerous\n
+Schmoomer, VirtualMan\n
 "
 infobox="${infobox}\n"
 
