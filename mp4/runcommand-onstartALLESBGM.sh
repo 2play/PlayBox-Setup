7480226c376d0f52a173eea460c9ee1576f9ad22
2play <2playbuilds@gmail.com>
2023-08-13 23:40:10 +0200

    lmp4 path correction

--- mp4/runcommand-onstartALLESBGM.sh
+++ mp4/runcommand-onstartALLESBGM.sh
@@ -1,5 +1,5 @@
 touch ~/.musicpaused.flag
-ifexist=`ls /home/pi/PlayBox-Setup/mp4/$1.mp4 |wc -l`
+ifexist=`ls /home/pi/lmp4/$1.mp4 |wc -l`
 if [[ $ifexist > 0 ]];then
 loader --blank /home/pi/lmp4/$1.mp4
 else
