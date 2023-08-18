e5793ef89d7b7cad572a60ebe15b528eba91b367
2play <sprafs@gmail.com>
2023-08-18 11:05:38 +0200

    Update runcommand-onstartALLESBGM.sh

--- mp4/runcommand-onstartALLESBGM.sh
+++ mp4/runcommand-onstartALLESBGM.sh
@@ -1,7 +1,7 @@
 touch ~/.musicpaused.flag
 ifexist=`ls /home/pi/lmp4/$1.mp4 |wc -l`
 if [[ $ifexist > 0 ]];then
-loader --blank /home/pi/lmp4/$1.mp4
+loader --blank /home/pi/lmp4/$1.mp4; touch ~/.musicpaused.flag;
 else
 loader --blank /home/pi/PlayBox-Setup/getready2play.mp4
 fi
