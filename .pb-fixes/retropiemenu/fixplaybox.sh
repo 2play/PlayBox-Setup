864bdb13d9d4ac8cac4638b79d626283dbce6e4e
2play <sprafs@gmail.com>
2023-08-21 17:01:01 +0200

    Update fixplaybox.sh

--- .pb-fixes/retropiemenu/fixplaybox.sh
+++ .pb-fixes/retropiemenu/fixplaybox.sh
@@ -3257,7 +3257,7 @@ function cl_cli_hist() {
 	cp $HOME/PlayBox-Setup/.pb-fixes/cli/.bash_history $HOME/
 	cd $HOME
 	#sed -i '1i***Welcome to PlayBox, 2Play!***\nsdl2-config --version\nmodetest -s 89:#0\nvulkaninfo | grep deviceName\nglxinfo -B\npython3 ~/code/export.py ~/RetroPie/roms/full_list.xlsx -d\nsudo raspi-config\nSkyscraper\nstartx\nglances\nbpytop\nsudo ~/RetroPie-Setup/retropie_setup.sh\nemulationstation\n2p-FixPlayBox' .bash_history
-	sed -i '14,1000d' .bash_history
+	sed -i '15,1000d' .bash_history
 	clear
 	echo
 	echo "[OK DONE!...]"
