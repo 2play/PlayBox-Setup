7b87ce2447b7141390cf0e78feabfd81215d1344
2play <sprafs@gmail.com>
2023-09-05 11:46:59 +0200

    Update post-fixes.sh

--- .pb-fixes/_scripts/post-fixes.sh
+++ .pb-fixes/_scripts/post-fixes.sh
@@ -334,31 +334,33 @@ clear
 # Mame2003_Plus Controller
 cd /opt/retropie/configs/arcade
 sed -i 's|^mame2003-plus_analog = "analog"|mame2003-plus_analog = "digital"|' retroarch-core-options.cfg;
+rm *.sh
+cd ~
 # Pico8 & DuckStation Standalone & Core
 sudo chown pi:pi -R /opt/retropie/emulators/pico8/
 sudo chmod 755 /opt/retropie/emulators/pico8/*
 #If user has Pico8 Disabled
-if [ -d ~/RetroPie/localroms/pico8.* ]; then
-echo "You have it disabled. We continue..."
-else
-	if [ -d ~/RetroPie/localroms ]; then
-	mkdir ~/RetroPie/localroms/pico8 && mkdir ~/addonusb/pico8
-	cd ~/RetroPie/localroms/pico8
-		if [ ! -f ~/RetroPie/localroms/pico8/+Start\ PICO8.sh ]; then wget https://github.com/2play/PBv2-PostFixes/raw/clean/home/pi/RetroPie/roms/pico8/%2BStart%20PICO8.sh
-		chmod 755 ~/RetroPie/localroms/pico8/+Start\ PICO8.sh
-		fi
-	else
-		if [ -d ~/RetroPie/roms/pico8.* ]; then
-			echo "You have it disabled. We continue..."
-			else
-			mkdir ~/RetroPie/roms/pico8 && cd ~/RetroPie/roms/pico8
-			if [ ! -f ~/RetroPie/roms/pico8/+Start\ PICO8.sh ]; then wget https://github.com/2play/PBv2-PostFixes/raw/clean/home/pi/RetroPie/roms/pico8/%2BStart%20PICO8.sh
-			chmod 755 ~/RetroPie/roms/pico8/+Start\ PICO8.sh
-			fi
-		fi
-	fi
-fi
-cd $HOME
+#if [ -d ~/RetroPie/localroms/pico8.* ]; then
+#echo "You have it disabled. We continue..."
+#else
+#	if [ -d ~/RetroPie/localroms ]; then
+#	mkdir ~/RetroPie/localroms/pico8 && mkdir ~/addonusb/pico8
+#	cd ~/RetroPie/localroms/pico8
+#		if [ ! -f ~/RetroPie/localroms/pico8/+Start\ PICO8.sh ]; then wget https://github.com/2play/PBv2-PostFixes/raw/clean/home/pi/RetroPie/roms/pico8/%2BStart%20PICO8.sh
+#		chmod 755 ~/RetroPie/localroms/pico8/+Start\ PICO8.sh
+#		fi
+#	else
+#		if [ -d ~/RetroPie/roms/pico8.* ]; then
+#			echo "You have it disabled. We continue..."
+#			else
+#			mkdir ~/RetroPie/roms/pico8 && cd ~/RetroPie/roms/pico8
+#			if [ ! -f ~/RetroPie/roms/pico8/+Start\ PICO8.sh ]; then wget https://github.com/2play/PBv2-PostFixes/raw/clean/home/pi/RetroPie/roms/pico8/%2BStart%20PICO8.sh
+#			chmod 755 ~/RetroPie/roms/pico8/+Start\ PICO8.sh
+#			fi
+#		fi
+#	fi
+#fi
+cd ~
 sudo chown pi:pi -R /opt/retropie/emulators/duckstation/
 sudo chmod 755 /opt/retropie/emulators/duckstation/*
 if ! grep -E 'duckstation = "XINIT:/opt/retropie/emulators/duckstation/duckstation-qt %ROM%"' /opt/retropie/configs/psx/emulators.cfg; then
