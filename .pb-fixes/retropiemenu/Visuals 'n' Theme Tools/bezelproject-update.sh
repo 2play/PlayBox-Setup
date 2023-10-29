3848450ef6ac5b2fc37ccd7cdf68d4565ab43c33
2play <2playbuilds@gmail.com>
2023-10-29 21:54:52 +0100

    Update bezelproject-update.sh

--- .pb-fixes/retropiemenu/Visuals 'n' Theme Tools/bezelproject-update.sh
+++ .pb-fixes/retropiemenu/Visuals 'n' Theme Tools/bezelproject-update.sh
@@ -5,11 +5,11 @@
 # 30.01.19
 
 cd /home/pi/RetroPie/retropiemenu/Visuals\ \'n\'\ Theme\ Tools/
-rm bezelproject.sh
+rm bezelproject.sh && rm /home/pi/RetroPie/retropiemenu/bezelproject.sh
 wget https://raw.githubusercontent.com/thebezelproject/BezelProject/master/bezelproject.sh
 chmod 755 bezelproject.sh
 
 cd /home/pi/PlayBox-Setup/.pb-fixes/retropiemenu/Visuals\ \'n\'\ Theme\ Tools/
-rm bezelproject.sh
+rm bezelproject.sh && rm /home/pi/RetroPie/retropiemenu/bezelproject.sh
 wget https://raw.githubusercontent.com/thebezelproject/BezelProject/master/bezelproject.sh
 chmod 755 bezelproject.sh
