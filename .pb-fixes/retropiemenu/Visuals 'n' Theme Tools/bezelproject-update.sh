06d53ab31438abfa32c06fa5cecf923c60338d2f
2play <sprafs@gmail.com>
2024-01-19 13:34:10 +0100

    Vanilla+ Conversion Part1

--- .pb-fixes/retropiemenu/Visuals 'n' Theme Tools/bezelproject-update.sh
+++ .pb-fixes/retropiemenu/Visuals 'n' Theme Tools/bezelproject-update.sh
@@ -4,12 +4,12 @@
 # Copyright (C)2018-2023 2Play! (S.R.)
 # 30.01.19
 
-cd /home/pi/RetroPie/retropiemenu/Visuals/
-rm bezelproject.sh
+cd /home/pi/RetroPie/retropiemenu/Visuals\ \'n\'\ Theme\ Tools/
+rm bezelproject.sh && rm /home/pi/RetroPie/retropiemenu/bezelproject.sh
 wget https://raw.githubusercontent.com/thebezelproject/BezelProject/master/bezelproject.sh
 chmod 755 bezelproject.sh
 
-cd /home/pi/PlayBox-Setup/.pb-fixes/retropiemenu/Visuals/
-rm bezelproject.sh
+cd /home/pi/PlayBox-Setup/.pb-fixes/retropiemenu/Visuals\ \'n\'\ Theme\ Tools/
+rm bezelproject.sh && rm /home/pi/RetroPie/retropiemenu/bezelproject.sh
 wget https://raw.githubusercontent.com/thebezelproject/BezelProject/master/bezelproject.sh
 chmod 755 bezelproject.sh
--- .pb-fixes/retropiemenu/Visuals 'n' Theme Tools/launchmp4.sh
+++ .pb-fixes/retropiemenu/Visuals 'n' Theme Tools/launchmp4.sh
@@ -65,8 +65,11 @@ function disable_mp4() {
 	if [ -f "/home/pi/.local/bin/esbgm" ]; then
 	rm /opt/retropie/configs/all/runcommand-onstart.sh;
 	cp /home/pi/PlayBox-Setup/mp4/runcommand-onstartESBGM.sh /opt/retropie/configs/all/runcommand-onstart.sh
+	cp /home/pi/PlayBox-Setup/mp4/runcommand-onendESBGM.sh /opt/retropie/configs/all/runcommand-onend.sh
+	rm /opt/retropie/configs/all/*.orig
 	else
 	rm /opt/retropie/configs/all/runcommand-onstart.sh
+	rm /opt/retropie/configs/all/runcommand-onend.sh
 	fi
 }
 
@@ -77,6 +80,7 @@ function enable_systems_mp4() {
 	cp /home/pi/PlayBox-Setup/mp4/runcommand-onstartALLESBGM.sh /opt/retropie/configs/all/runcommand-onstart.sh
 	else
 	cp /home/pi/PlayBox-Setup/mp4/runcommand-onstartALL.sh /opt/retropie/configs/all/runcommand-onstart.sh
+	fi
 }
 
 main_menu
