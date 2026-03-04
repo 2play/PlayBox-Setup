0e38186101e39f4c9618dccdc951780cb928d4bb
2play <sprafs@gmail.com>
2026-03-04 16:44:46 +0100

    Copyright (C)2018-2026

--- .pb-fixes/retropiemenu/fixplaybox.sh
+++ .pb-fixes/retropiemenu/fixplaybox.sh
@@ -2,7 +2,7 @@
 # All required fixes in case you break something 
 # Fix retropiemenu, es_systems.cfg etc.
 # The PlayBox Project
-# Copyright (C)2018-2024 2Play! (S.R.)+
+# Copyright (C)2018-2026 2Play! (S.R.)+
 # PlayBox ToolKit
 
 pb_version="PlayBox ToolKit Version 2.0 Dated 05.2024"
@@ -1537,7 +1537,7 @@ function skyscraper() {
 function mesa_vk() {
 # Install Pi4 Igalia Mesa Vulkan (v3dv-conformance-1.0) Driver https://blogs.igalia.com/apinheiro/
 # The PlayBox Project
-# Copyright (C)2018-2024 2Play! (S.R.)
+# Copyright (C)2018-2026 2Play! (S.R.)
 # 10.05.2023
 	dialog --backtitle "PlayBox Toolkit" \
 	--title "MESA & VULKAN OPTIONS MENU" \
@@ -1969,7 +1969,7 @@ function pikiss_git() {
 function rpc80_saves() {
 # Based on RPC80 Single Saves Folder Script
 # The PlayBox Project
-# Copyright (C)2018-2024 2Play! (S.R.)
+# Copyright (C)2018-2026 2Play! (S.R.)
 # 23.07.20
 	dialog --backtitle "PlayBox Toolkit" \
 	--title "RPC80 SINGLE SAVES DIR OPTIONS MENU" \
@@ -3753,7 +3753,7 @@ function fwup_pi4() {
 function sysinfo() {
 	dialog --infobox "...Please Wait..." 3 22 ; sleep 1
 # The PlayBox Project
-# Copyright (C)2018-2024 2Play! (S.R.)
+# Copyright (C)2018-2026 2Play! (S.R.)
 # 26.03.2022
 	clear
 echo "
