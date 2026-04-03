483a8c60996fd84d18f2ea84c9bfb3949aa4a9c1
2play <sprafs@gmail.com>
2026-04-03 18:24:07 +0200

    Update thankyou.sh

--- .pb-fixes/_scripts/thankyou.sh
+++ .pb-fixes/_scripts/thankyou.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # The PlayBox Project
 # Copyright (C)2018-2026 2Play! (S.R.)
-# 03.2024
+# 04.2026
 
 infobox=""
 infobox="${infobox}\n*** 2Play! Top Supporters & Friends***:\n- Jahitu, jucosorin, VGMonkey, scaled, BMP, Tisusbr, Yelah05, TechXero, bluestang, Randyslim, Traxis, Strunx, ChrisKerkides, Mrnebu, Edale, Gomer, QUIZASERAQ, dbresson, Everblight82, Eliasblizzard, Sunrise169, Grumpa and of course any/all MEGA supporters!\n\n
 
 *** Developers & Scripts ***:\n
-Rpc80 (Ryan Connors), Rydra, EasyHax (aka Forrest), Demetris Ierokipides aka Ntemis, CyperGhost, Meleu, Lars Muldjord (Skyskraper), David Marti (Motion Blue), Bezel Project Team, Folly, Valerino, Aditya Shakya aka adi1090x, Paul Cercueil aka pcercuei.\n\n
+Rpc80 (Ryan Connors), Rydra, EasyHax (aka Forrest), Demetris Ierokipides aka Ntemis, CyperGhost, Meleu, Lars Muldjord (Skyskraper), David Marti (Motion Blue), Bezel Project Team, Folly, Valerino, Aditya Shakya aka adi1090x, Paul Cercueil aka pcercuei, MS Copilot.\n\n
 
 *** OS & FrontEnd ***:\n
 Armbian Team for the nice clean OS!.\n
-Retropie Team (Retropie), Andrew Mickelson (Attract Mode) & Radek Dutkiewicz aka oomek (Attract Plus), HyperPie Team (HyperPie), Mátyás Mustoha (Pegasus-FE).\n\n
+Retropie Team (Retropie), Andrew Mickelson (Attract Mode) & Radek Dutkiewicz aka oomek (Attract Mode Plus), HyperPie Team (HyperPie), Mátyás Mustoha (Pegasus-FE).\n\n
 
 *** Themes & Art Ideas ***:\n
 Chicuelo & c64-dev, GeekOB, Ricky Romero, TreyM, Mr_RetroLust, Rachid Lotf and all devianart\art creators.\n\n
