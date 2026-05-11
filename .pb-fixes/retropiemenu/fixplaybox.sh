b78f37fee4f75b8a82ca79180712018fdbf2b72a
2play <sprafs@gmail.com>
2026-05-11 10:26:04 +0200

    Update fixplaybox.sh

--- .pb-fixes/retropiemenu/fixplaybox.sh
+++ .pb-fixes/retropiemenu/fixplaybox.sh
@@ -4,7 +4,7 @@
 # Copyright (C)2018-2026 2Play! (S.R.)+
 # PlayBox ToolKit
 BACKTITLE="PLAYBOX PROJECT"
-pb_version="PlayBox ToolKit Version 2.0 Dated 08.05.2026"
+pb_version="PlayBox ToolKit Version 2.0 Dated 11.05.2026"
 
 infobox=""
 infobox="${infobox}\n\n\n\n\n"
