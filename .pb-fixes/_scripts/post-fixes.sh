31257ee2fe418b8951fc71a486d4a14b7cbc3669
2play <sprafs@gmail.com>
2026-05-15 18:07:28 +0200

    Manual ES Restart Preferred

--- .pb-fixes/_scripts/post-fixes.sh
+++ .pb-fixes/_scripts/post-fixes.sh
@@ -716,31 +716,32 @@ function enable_core_cfg() {
 
 function restart_es() {
     clear
-    #echo "[Restarting EmulationStation...]"
     echo "[WARN] Please restart EmulationStation manually or Reboot for changes to take effect..."
+    
+	#echo "[Restarting EmulationStation...]"
     #sleep 1
-
-    # Stop ES
-    #pkill -f emulationstation
+ 
+    # Detect if running locally or via SSH
+	#	if [[ -n "$SSH_CONNECTION" ]]; then
+		
+	#	echo "[WARN] Restart skipped: Script running under SSH session]"
+	#	echo "Please restart ES manually on the actual console."
+	#else
+	# Stop ES
+	#	pkill -f emulationstation
 
     # Wait until ES is really gone
-    #while pgrep -f emulationstation >/dev/null; do
-    #    sleep 2
-    #done
+	#	while pgrep -f emulationstation >/dev/null; do sleep 2; done
 
     # Extra delay to let esbgm service react
-    #sleep 1
-
-    # Detect if running locally or via SSH
-    #if [[ $(tty) == /dev/tty* ]]; then
-        # Relaunch ES bound to the local console
-    #   nohup emulationstation --no-splash >/dev/tty 2>&1 &
-    #    disown
-    #    echo "[EmulationStation restarted locally]"
-    #else
-    #    echo "[WARN] Restart skipped: Script running under SSH session]"
-    #    echo "Please restart ES manually on the actual console."
-    #fi
+	#	sleep 1
+	
+    # Relaunch ES bound to the local console
+	#	nohup emulationstation --no-splash >/dev/null 2>&1 &
+	#	disown
+	#	echo "[EmulationStation restarted successfully]"
+	#	fi
+	
 	pausepress
 }
 
